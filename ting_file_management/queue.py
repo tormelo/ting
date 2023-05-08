9a40f5e634332e56aa6216e8cec793460ff52c4a
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-08 15:00:25 -0300

    feat: implements Queue class

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,27 @@
 from ting_file_management.abstract_queue import AbstractQueue
+from collections import deque
 
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = deque()
+        self._length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self._length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
+        self._length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        removed_element = self._data.popleft()
+        if removed_element:
+            self._length -= 1
+            return removed_element
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= self._length or index < 0:
+            raise IndexError("Índice Inválido ou Inexistente")
+
+        return self._data[index]
