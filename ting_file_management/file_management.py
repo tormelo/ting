76b61e333141c290f363f693847f483f84084561
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-08 15:12:43 -0300

    feat: add txt_importer function to the file_management module

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if ".txt" not in path_file:
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as my_file:
+            return my_file.read().split("\n")
+    except OSError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
