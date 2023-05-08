b30b109c6618a901ca60f653e857bbcd5c3fb522
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-08 17:33:42 -0300

    feat: add exists_word function to the word_search module

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,29 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    word_lower = word.lower()
+
+    results = []
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        lines = file_data["linhas_do_arquivo"]
+
+        occurrences = []
+        for j in range(len(lines)):
+            if word_lower in lines[j].lower():
+                occurrences.append({"linha": j + 1})
+
+        if len(occurrences):
+            results.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_data["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return results
 
 
 def search_by_word(word, instance):
