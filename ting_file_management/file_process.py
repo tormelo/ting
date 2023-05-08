fa996c6e80104a5b6d1c9ba1d128c1af32b20fa9
tormelo <23294699+tormelo@users.noreply.github.com>
2023-05-08 15:34:23 -0300

    feat: add process function to the file_process module

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+import sys
+
+
+def process(path_file, instance: Queue):
+    lines = txt_importer(path_file)
+
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    data_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(data_dict)
+
+    print(data_dict, file=sys.stdout)
 
 
 def remove(instance):
